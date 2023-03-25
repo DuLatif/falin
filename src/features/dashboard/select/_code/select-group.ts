5eae1d9a0c84deb24b59f53e509ed24dc2bd096d
Abdul Latif Mubasir <bj.angel119@gmail.com>
2023-03-26 03:21:22 +0700

    create code snippet for select component

---
+++ src/features/dashboard/select/_code/select-group.ts
@@ -0,0 +1,50 @@
+const baseContainer = (code: string) => {
+  return `import React from "react";
+import { Box } from "@mui/material";
+import SelectGroup from "@/components/SelectGroup";
+
+const listOptions = [
+  { label: "Islam", value: "Islam" },
+  { label: "Kristen", value: "Kristen" },
+  { label: "Katholik", value: "Katholik" },
+  { label: "Budha", value: "Budha" },
+];
+
+const App : React.FC = () => {
+  return (
+    <Box>
+      ${code}
+    </Box>
+  )
+}`;
+};
+
+export const selectGroupBasicCode = baseContainer(
+  `<SelectGroup label="Basic Select" options={listOptions} />`
+);
+
+export const selectGroupWithPlaceholderCode = baseContainer(`<SelectGroup
+        label="Select with placeholder"
+        options={listOptions}
+        helperText={"This is a helper text"}
+      />`);
+
+export const selectGroupRequiredCode = baseContainer(
+  `<SelectGroup label="Required Select" options={listOptions} required />`
+);
+
+export const selectGroupErrorCode = baseContainer(`<SelectGroup
+        label="Select Error"
+        options={listOptions}
+        required
+        error
+        helperText="This is a error message"
+      />`);
+
+export const selectGroupDisabled = baseContainer(`<SelectGroup
+        label="Disabled Select"
+        options={listOptions}
+        required
+        disabled
+        helperText="This is a error message"
+      />`);
--- src/features/dashboard/select/index.tsx
+++ src/features/dashboard/select/index.tsx
@@ -1,90 +1,21 @@
-import SelectGroup from "@/components/SelectGroup";
-import {
-  Box,
-  Card,
-  FormControl,
-  FormHelperText,
-  Grid,
-  FormLabel,
-  MenuItem,
-  Select,
-  Typography,
-} from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import React from "react";
+import BasicSelectGuide from "./BasicSelect";
+import SelectGroupGuide from "./SelectGroup";
 
 const SelectGuide: React.FC = () => {
   return (
     <Box>
       <Typography variant="h6">Select Guide</Typography>
       <Box mt={4}>
-        <Card>
-          <Grid container spacing={2}>
-            <Grid item md={4}>
-              <FormControl>
-                <FormLabel htmlFor="religion">Religion</FormLabel>
-                <Select defaultValue={" "} id="religion">
-                  <MenuItem value=" ">-- Religion ---</MenuItem>
-                  <MenuItem value="Islam">Islam</MenuItem>
-                  <MenuItem value="Kristen">Kristen</MenuItem>
-                  <MenuItem value="Katholik">Katholik</MenuItem>
-                  <MenuItem value="Budha">Budha</MenuItem>
-                  <MenuItem value="Hindu">Hindu</MenuItem>
-                </Select>
-                <FormHelperText>This is an error message</FormHelperText>
-              </FormControl>
-            </Grid>
-            <Grid item md={4}>
-              <SelectGroup
-                label="Religion"
-                required
-                options={[
-                  { label: "Islam", value: "Islam" },
-                  { label: "Kristen", value: "Kristen" },
-                  { label: "Katholik", value: "Katholik" },
-                  { label: "Budha", value: "Budha" },
-                ]}
-              />
-            </Grid>
-            <Grid item md={4}>
-              <SelectGroup
-                label="Religion"
-                placeholder="Select Religion"
-                options={[
-                  { label: "Islam", value: "Islam" },
-                  { label: "Kristen", value: "Kristen" },
-                  { label: "Katholik", value: "Katholik" },
-                  { label: "Budha", value: "Budha" },
-                ]}
-              />
-            </Grid>
-            <Grid item md={4}>
-              <SelectGroup
-                label="Religion"
-                disabled
-                value="Budha"
-                options={[
-                  { label: "Islam", value: "Islam" },
-                  { label: "Kristen", value: "Kristen" },
-                  { label: "Katholik", value: "Katholik" },
-                  { label: "Budha", value: "Budha" },
-                ]}
-              />
-            </Grid>
-            <Grid item md={4}>
-              <SelectGroup
-                label="Religion"
-                readOnly
-                value={"Islam"}
-                options={[
-                  { label: "Islam", value: "Islam" },
-                  { label: "Kristen", value: "Kristen" },
-                  { label: "Katholik", value: "Katholik" },
-                  { label: "Budha", value: "Budha" },
-                ]}
-              />
-            </Grid>
+        <Grid container spacing={4}>
+          <Grid item md={12}>
+            <BasicSelectGuide />
           </Grid>
-        </Card>
+          <Grid item md={12}>
+            <SelectGroupGuide />
+          </Grid>
+        </Grid>
       </Box>
     </Box>
   );
