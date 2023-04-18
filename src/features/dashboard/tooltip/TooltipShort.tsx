68482ed69bb83ad72f1a3439a0c32fc48d0d42e3
Abdul Latif Mubasir <bj.angel119@gmail.com>
2023-04-18 14:48:22 +0700

    create tooltip component docs

---
+++ src/features/dashboard/tooltip/TooltipShort.tsx
@@ -0,0 +1,31 @@
+import CodeSnippet from "@/components/CodeSnippet"
+import { Card, Stack, Tooltip, Typography } from "@mui/material"
+import React from "react"
+import { tooltipShortCode } from "./_code/tooltip-short"
+
+const TooltipShort: React.FC = () => {
+  return (
+    <Card component="section">
+      <Typography variant="subtitle1" fontWeight={"semiBold"} mb={1}>
+        Default Tooltip
+      </Typography>
+      <Stack direction="row" spacing={4}>
+        <Tooltip title="This is a tooltip">
+          <Typography>Tooltip Top</Typography>
+        </Tooltip>
+        <Tooltip title="This is a tooltip" placement="bottom">
+          <Typography>Tooltip Bottom</Typography>
+        </Tooltip>
+        <Tooltip title="This is a tooltip" placement="left">
+          <Typography>Tooltip Left</Typography>
+        </Tooltip>
+        <Tooltip title="This is a tooltip" placement="right">
+          <Typography>Toolti Right</Typography>
+        </Tooltip>
+      </Stack>
+      <CodeSnippet code={tooltipShortCode} />
+    </Card>
+  )
+}
+
+export default TooltipShort
