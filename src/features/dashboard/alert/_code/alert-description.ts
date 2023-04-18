ee9c4d425a9138a9d66d2b84e193830127217cf3
Abdul Latif Mubasir <bj.angel119@gmail.com>
2023-04-18 13:53:22 +0700

    create alert description docs

---
+++ src/features/dashboard/alert/_code/alert-description.ts
@@ -0,0 +1,63 @@
+export const alertDescription = `import React from "react";
+import { Alert, AlertTitle, Card, IconButton, Stack, Typography } from "@mui/material"
+import { XCircle } from "phosphor-react"
+
+const App : React.FC = () => {
+  return (
+    <Card component="section">
+      <Typography variant="subtitle1" fontWeight={"semiBold"} mb={1}>
+        Alert Description
+      </Typography>
+      <Stack direction="column" spacing={1} mb={2}>
+        <Alert
+          variant="outlined"
+          severity="error"
+          action={
+            <IconButton color="error">
+              <XCircle size={24} weight="bold" />
+            </IconButton>
+          }
+        >
+          <AlertTitle>There was a problem with that action</AlertTitle>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.
+        </Alert>
+        <Alert
+          variant="outlined"
+          severity="warning"
+          action={
+            <IconButton color="warning">
+              <XCircle size={24} weight="bold" />
+            </IconButton>
+          }
+        >
+          <AlertTitle>Just to let you know this might be a problem</AlertTitle>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.
+        </Alert>
+        <Alert
+          variant="outlined"
+          severity="info"
+          action={
+            <IconButton color="info">
+              <XCircle size={24} weight="bold" />
+            </IconButton>
+          }
+        >
+          <AlertTitle>We&apos;ve just released a new feature</AlertTitle>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.
+        </Alert>
+        <Alert
+          variant="outlined"
+          severity="success"
+          action={
+            <IconButton color="success">
+              <XCircle size={24} weight="bold" />
+            </IconButton>
+          }
+        >
+          <AlertTitle>Successfully updated profile</AlertTitle>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.
+        </Alert>
+      </Stack>
+    </Card>
+  )
+}`
--- src/features/dashboard/alert/index.tsx
+++ src/features/dashboard/alert/index.tsx
@@ -1,6 +1,6 @@
-import { Box, Card, Typography, Alert, Stack, IconButton, AlertTitle, Grid } from "@mui/material"
+import { Box, Grid, Typography } from "@mui/material"
 import React from "react"
-import { XCircle } from "phosphor-react"
+import AlertDescription from "./AlertDescription"
 import AlertFilled from "./AlertFilled"
 import AlertOutlined from "./AlertOutlined"
 
@@ -16,91 +16,11 @@ const AlertGuide: React.FC = () => {
           <Grid item md={6}>
             <AlertOutlined />
           </Grid>
+          <Grid item md={12}>
+            <AlertDescription />
+          </Grid>
         </Grid>
       </Box>
-      <Card sx={{ mt: 4 }}>
-        <Stack direction="column" spacing={1} mb={2}>
-          <Typography variant="subtitle1">Alert Filled</Typography>
-          <Alert variant="filled" severity="error">
-            This is an error alert — check it out!
-          </Alert>
-          <Alert variant="filled" severity="warning">
-            This is an error alert — check it out!
-          </Alert>
-          <Alert variant="filled" severity="info">
-            This is an error alert — check it out!
-          </Alert>
-          <Alert variant="filled" severity="success">
-            This is an error alert — check it out!
-          </Alert>
-        </Stack>
-        <Stack direction="column" spacing={1} mb={2}>
-          <Typography variant="subtitle1">Alert Outlined</Typography>
-          <Alert variant="outlined" severity="error">
-            This is an error alert — check it out!
-          </Alert>
-          <Alert variant="outlined" severity="warning">
-            This is an error alert — check it out!
-          </Alert>
-          <Alert variant="outlined" severity="info">
-            This is an error alert — check it out!
-          </Alert>
-          <Alert variant="outlined" severity="success">
-            This is an error alert — check it out!
-          </Alert>
-        </Stack>
-        <Stack direction="column" spacing={1} mb={2}>
-          <Typography variant="subtitle1">Alert Description</Typography>
-          <Alert
-            variant="outlined"
-            severity="error"
-            action={
-              <IconButton color="error">
-                <XCircle size={24} weight="bold" />
-              </IconButton>
-            }
-          >
-            <AlertTitle>There was a problem with that action</AlertTitle>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.
-          </Alert>
-          <Alert
-            variant="outlined"
-            severity="warning"
-            action={
-              <IconButton color="warning">
-                <XCircle size={24} weight="bold" />
-              </IconButton>
-            }
-          >
-            <AlertTitle>Just to let you know this might be a problem</AlertTitle>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.
-          </Alert>
-          <Alert
-            variant="outlined"
-            severity="info"
-            action={
-              <IconButton color="info">
-                <XCircle size={24} weight="bold" />
-              </IconButton>
-            }
-          >
-            <AlertTitle>We&apos;ve just released a new feature</AlertTitle>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.
-          </Alert>
-          <Alert
-            variant="outlined"
-            severity="success"
-            action={
-              <IconButton color="success">
-                <XCircle size={24} weight="bold" />
-              </IconButton>
-            }
-          >
-            <AlertTitle>Successfully updated profile</AlertTitle>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.
-          </Alert>
-        </Stack>
-      </Card>
     </Box>
   )
 }
