0db3c1bb4c131a7ecb45c73bad441ecf29724e9e
Abdul Latif Mubasir <bj.angel119@gmail.com>
2023-08-25 16:03:35 +0700

    feat: add slider with custom navigation style

--- src/features/dashboard/slider/_code/slider-splide.ts
+++ src/features/dashboard/slider/_code/slider-splide.ts
@@ -63,3 +63,75 @@ const SliderGuide: React.FC = () => {
 
 export default SliderGuide
 `
+
+export const splideCustomNavigationCode = `import { Box, Card, Typography,Stack,IconButton } from "@mui/material"
+import React , {useRef} from "react"
+import { Splide, SplideSlide } from "@splidejs/react-splide"
+
+// Default theme
+import "@splidejs/react-splide/css"
+
+// or other themes
+// import "@splidejs/react-splide/css/skyblue"
+// import "@splidejs/react-splide/css/sea-green"
+
+const SliderGuide: React.FC = () => {
+  const splideRef = useRef<Splide | null>(null)
+  return (
+    <Box mt={6}>
+      <Typography variant="subtitle1" mb={2} fontWeight={"semiBold"}>
+        Custom Navigation
+      </Typography>
+      <Splide
+        aria-label="My Favorite Images"
+        ref={splideRef}
+        options={{
+          rewind: true,
+          gap: spacing[8],
+          padding: "5rem",
+          perPage: 3,
+          perMove: 1,
+          autoplay: true,
+          pagination: false,
+          arrows: false,
+        }}
+      >
+        <SplideSlide>
+          <Box sx={styles.box}>1</Box>
+        </SplideSlide>
+        <SplideSlide>
+          <Box sx={styles.box}>2</Box>
+        </SplideSlide>
+        <SplideSlide>
+          <Box sx={styles.box}>3</Box>
+        </SplideSlide>
+        <SplideSlide>
+          <Box sx={styles.box}>4</Box>
+        </SplideSlide>
+        <SplideSlide>
+          <Box sx={styles.box}>5</Box>
+        </SplideSlide>
+        <SplideSlide>
+          <Box sx={styles.box}>6</Box>
+        </SplideSlide>
+        <SplideSlide>
+          <Box sx={styles.box}>7</Box>
+        </SplideSlide>
+        <SplideSlide>
+          <Box sx={styles.box}>8</Box>
+        </SplideSlide>
+      </Splide>
+      <Stack direction="row" justifyContent={"center"} spacing={6} sx={{ mt: 4 }}>
+        <IconButton onClick={() => splideRef.current?.go("-1")}>
+          <ArrowLeft />
+        </IconButton>
+        <IconButton onClick={() => splideRef.current?.go("+1")}>
+          <ArrowRight />
+        </IconButton>
+      </Stack>
+    </Box>
+  )
+}
+
+export default SliderGuide
+`
--- src/features/dashboard/slider/index.tsx
+++ src/features/dashboard/slider/index.tsx
@@ -1,17 +1,36 @@
-import { Box, Card, Typography } from "@mui/material"
-import React from "react"
+import { Box, Card, IconButton, Stack, SxProps, Typography } from "@mui/material"
+import React, { useRef } from "react"
 import { Splide, SplideSlide } from "@splidejs/react-splide"
+
+const styles: { box: SxProps } = {
+  box: {
+    width: "300px",
+    height: "400px",
+    background: neutral[400],
+    color: "#fff",
+    fontSize: "48px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontWeight: "bold",
+  },
+}
 
 // Default theme
 import "@splidejs/react-splide/css"
 import CodeSnippet from "@/components/CodeSnippet"
-import { splideSliderCode } from "./_code/slider-splide"
+import { splideCustomNavigationCode, splideSliderCode } from "./_code/slider-splide"
+import { spacing } from "@/theme/spacing"
+import { neutral } from "@/theme/colors"
+import { ArrowLeft, ArrowRight } from "phosphor-react"
 
 // or other themes
 // import "@splidejs/react-splide/css/skyblue"
 // import "@splidejs/react-splide/css/sea-green"
 
 const SliderGuide: React.FC = () => {
+  const splideRef = useRef<Splide | null>(null)
+
   return (
     <Box>
       <Typography variant="h6">Slider Guide</Typography>
@@ -64,6 +83,59 @@ const SliderGuide: React.FC = () => {
             </SplideSlide>
           </Splide>
           <CodeSnippet code={splideSliderCode} />
+          <Box mt={6}>
+            <Typography variant="subtitle1" mb={2} fontWeight={"semiBold"}>
+              Custom Navigation
+            </Typography>
+            <Splide
+              aria-label="My Favorite Images"
+              ref={splideRef}
+              options={{
+                rewind: true,
+                gap: spacing[8],
+                padding: "5rem",
+                perPage: 3,
+                perMove: 1,
+                autoplay: true,
+                pagination: false,
+                arrows: false,
+              }}
+            >
+              <SplideSlide>
+                <Box sx={styles.box}>1</Box>
+              </SplideSlide>
+              <SplideSlide>
+                <Box sx={styles.box}>2</Box>
+              </SplideSlide>
+              <SplideSlide>
+                <Box sx={styles.box}>3</Box>
+              </SplideSlide>
+              <SplideSlide>
+                <Box sx={styles.box}>4</Box>
+              </SplideSlide>
+              <SplideSlide>
+                <Box sx={styles.box}>5</Box>
+              </SplideSlide>
+              <SplideSlide>
+                <Box sx={styles.box}>6</Box>
+              </SplideSlide>
+              <SplideSlide>
+                <Box sx={styles.box}>7</Box>
+              </SplideSlide>
+              <SplideSlide>
+                <Box sx={styles.box}>8</Box>
+              </SplideSlide>
+            </Splide>
+            <Stack direction="row" justifyContent={"center"} spacing={6} sx={{ mt: 4 }}>
+              <IconButton onClick={() => splideRef.current?.go("-1")}>
+                <ArrowLeft />
+              </IconButton>
+              <IconButton onClick={() => splideRef.current?.go("+1")}>
+                <ArrowRight />
+              </IconButton>
+            </Stack>
+            <CodeSnippet code={splideCustomNavigationCode} />
+          </Box>
         </Card>
       </Box>
     </Box>
